beebaef780d526289b865f658f70fd252181d6d3
Luqman-Muhammed <129735104+Luqman-Muhammed@users.noreply.github.com>
2023-09-23 01:12:51 +0530

    main.py

--- main.py
+++ main.py
@@ -4,56 +4,47 @@ import subprocess
 import pywhatkit
 import pyttsx3
 import webbrowser
-
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 recognizer = sr.Recognizer()
-
-
 def cmd():
-
     global text
     with sr.Microphone() as source:
-        print('Clearing..... bg noises')
+        print('Clearing background noises...')
+        engine.say("Clearing background noises...")
+        engine.runAndWait()
         recognizer.adjust_for_ambient_noise(source, duration=0.5)
-        print('Ask me....')
-        recordedaudio = recognizer.listen(source)
-
+        print('Ask me something...')
+        engine.say("Ask me something...")
+        engine.runAndWait()
+        recorded_audio = recognizer.listen(source)
     try:
-        text = recognizer.recognize_google(recordedaudio, language='en_US')
+        text = recognizer.recognize_google(recorded_audio, language='en-US')
         text = text.lower()
         print('Your message: ', format(text))
-
     except:
         pass
-
     if 'google' in text:
-        a = 'opening google...'
-        engine.say(a)
-        engine.runAndWait()
         webbrowser.open('www.google.com')
-    if 'time' in text:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        print(time)
-        engine.say(time)
+        engine.say('Opening Google')
         engine.runAndWait()
-    if 'play' in text:
-        b = 'Opening youtube...'
-        engine.say(b)
+    elif 'time' in text:
+        current_time = datetime.datetime.now().strftime('%I:%M %p')
+        print(current_time)
+        engine.say(current_time)
         engine.runAndWait()
+    elif 'play' in text:
         pywhatkit.playonyt(text)
-    if 'youtube' in text:
-        c = 'Opening youtube...'
-        engine.say(c)
+        engine.say('Playing ' + text)
         engine.runAndWait()
+    elif 'youtube' in text:
         webbrowser.open('www.youtube.com')
-    if 'bye' in text:
-        d = 'see you later'
-        engine.say(d)
+        engine.say('Opening YouTube')
         engine.runAndWait()
-
-
+    elif 'bye' in text:
+        engine.say('See you later')
+        engine.runAndWait()
+        exit()
 while True:
     cmd()
-
